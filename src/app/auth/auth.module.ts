0531ce33536f3eaa3d7f2666235b67d655ede013
GMartinez0210 <genaro010204@gmail.com>
2023-10-07 00:22:21 -0500

    Login and register view done

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    LoginModule,
+    RegisterModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    LoginModule,
+    RegisterModule
+  ]
+})
+export class AuthModule { }
