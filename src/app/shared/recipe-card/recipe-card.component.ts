143b12614c8ce19db987aaf0c927357466f8027f
AngeloBalbinx <balbinangelo94@gmail.com>
2023-11-16 02:04:34 -0500

    Avance de listado de recetas filtrado

--- src/app/shared/recipe-card/recipe-card.component.ts
+++ src/app/shared/recipe-card/recipe-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-card',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-card.component.css']
 })
 export class RecipeCardComponent {
+  @Input() receta: any;
   cardImage = "./assets/LogoFoodmaker.PNG"
-  cardName = "Pizza Pepperoni"
-  cookingTime = "1h"
+
 }
--- src/app/shared/recipe-cards/recipe-cards.component.ts
+++ src/app/shared/recipe-cards/recipe-cards.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecetasService } from 'src/app/service/receta.service';
 
 @Component({
   selector: 'app-recipe-cards',
   templateUrl: './recipe-cards.component.html',
   styleUrls: ['./recipe-cards.component.css']
 })
-export class RecipeCardsComponent {
-
+export class RecipeCardsComponent implements OnInit{
+  @Input() receta:any;
+  tiempoPreparacion:number=0;
+  recetas: any[] = [];
+  constructor(private route:ActivatedRoute,private recetaService:RecetasService){}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params =>{
+      this.tiempoPreparacion = +params['tiempoPreparacion'] || 0;
+      this.recetaService.obtenerRecetasPorTiempo(this.tiempoPreparacion).subscribe((recetas:any[]) =>{
+        this.recetas = recetas;
+       
+      },
+      error =>{
+        console.error('Error al obtener las recetas',error)
+      });
+    })
+  }
 }
